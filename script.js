8cb7065988c555354319a05b026e8235424ece4c
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-01 19:19:48 -0400

    check uncheck added

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
 
 
-	// function addNewItem() {
-	// 	newItem.text()
-	// 	$(this).append(newItem);
-	// }
-
 	$("#add").on("click", function() {
 		var newItemName = $("#input").val();
 		var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div><div class="text">Uncat item li</div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div>';
@@ -23,9 +18,16 @@ $(document).ready(function() {
 
 	$(".remove").on("click", 
 		function() {
-		$(this).closest(".item").remove();
+			console.log("remove triggered");
+			$(this).closest(".item").remove();
 		}
 	);
 
+	$(".check-uncheck").on("click", 
+		function() {
+			console.log("check-uncheck triggered");
+			$(this).closest(".item").toggleClass("checked");
+		}
+	);
 
 });
