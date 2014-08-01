777de37a389c68f15ad7539c9fe08656993e9d2e
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-01 19:54:59 -0400

    sync commit
    nothing new really

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 	$("#add").on("click", function() {
 		var newItemName = $("#input").val();
-		var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div><div class="text">Uncat item li</div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div>';
+		var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div><div class="text">newItemName goes here</div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div>';
 
 		$("#uncategorized-ul").append(newItem);
 
@@ -15,6 +15,27 @@ $(document).ready(function() {
 		$("#input").val("");
 	});
 
+	// $("#add").on("click", function() {
+	// 	var newItemName = $("#input").val();
+	// 	var newLi = document.createElement("li");
+	// 	function newDiv(brian) {
+	// 		var newDiv = document.createElement("div");
+	// 		$(newDiv).addClass(brian);
+	// 	};
+	// 	// $(newLi).text(newItemName);
+	// 	newDiv("remove");
+
+	// 	$(newLi).append($(newDiv));
+
+	// 	$("#uncategorized-ul").append($(newLi));
+
+	// 	var lastItem = $("#uncategorized-ul").last();
+
+	// 	$(lastItem).find(".text").text(newItemName);
+	// 	console.log( lastItem );
+
+	// 	$("#input").val("");
+	// });
 
 	$(".remove").on("click", 
 		function() {
