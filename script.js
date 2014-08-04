d9b1072006c15c521aa20a23d39f8fe3859e0c6e
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-04 12:00:12 -0400

    Revert "broken"
    This reverts commit d5a81f7d6da0b275f59872f4350bd0acde449f17.

--- script.js
+++ script.js
@@ -10,17 +10,11 @@ $(document).ready(function() {
 		lastItem.find(".text").text(newItemName);
 	}
 
-	// function changeCatTo(categoryId) {
-		// var x = $(this);
-		// var lineItem = x.closest("li");
-		// lineItem.remove();
-		// categoryId.append(lineItem);
-		// var lineItem = $(this).closest("li");
-		// lineItem.remove();
-		// $(categoryId).append(lineItem);	
-
-
-	}
+	// function changeCat(categoryId) {
+	// 	var lineItem = $(this).closest("li");
+	// 	lineItem.remove();
+	// 	$(categoryId).append(lineItem);	
+	// }
 
 	$("header .cat-icons").on("click", "i", function(){
 		$(".cat-icons i").removeClass("cat-select");
@@ -30,13 +24,41 @@ $(document).ready(function() {
 		};
 	});
 
-	$("li .cat-icons").on("click", "i", function() {
+	$("li .cat-icons").on("click", "i", function(){
 		var lineItem = $(this).closest("li");
 		lineItem.remove();
 		$("#fnv-ul").append(lineItem);	
+
+		// if($(this).closest("li").find("i").first().hasClass("cat-select")) {
+		// 	$("#fnv-ul").append(lineItem);
+
+
+		// };
+
+
+		// 	("li .cat-icons i:first-child").hasClass("cat-select")) {
+		// 	addNewItem("#fnv-ul");
+		// } else if($("header .cat-icons i:nth-child(2)").hasClass("cat-select")) {
+		// 	addNewItem("#dairy-ul");
+		// } else if($("header .cat-icons i:nth-child(3)").hasClass("cat-select")) {
+		// 	addNewItem("#meat-ul");
+		// } else if($("header .cat-icons i:nth-child(4)").hasClass("cat-select")) {
+		// 	addNewItem("#bng-ul");
+		// } else if($("header .cat-icons i:nth-child(5)").hasClass("cat-select")) {
+		// 	addNewItem("#snacks-ul");
+		// } else if($("header .cat-icons i:nth-child(6)").hasClass("cat-select")) {
+		// 	addNewItem("#canned-ul");
+		// } else if($("header .cat-icons i:nth-child(7)").hasClass("cat-select")) {
+		// 	addNewItem("#spices-ul");
+		// } else if($("header .cat-icons i:nth-child(8)").hasClass("cat-select")) {
+		// 	addNewItem("#hhg-ul");
+		// } else if($("header .cat-icons i:nth-child(9)").hasClass("cat-select")) {
+		// 	addNewItem("#other-ul");
+		// } 
+
+		// $("li .cat-icons i").removeClass("cat-select");
+
 	});
-
-
 
 	$("#add").on("click", function() {
 		if($("header i").hasClass("cat-select") === false) {
@@ -77,6 +99,33 @@ $(document).ready(function() {
 			$("#add").click();
 		};
 	});
+
+
+
+
+
+
+	// $("#add").on("click", function() {
+	// 	var newItemName = $("#input").val();
+	// 	var newLi = document.createElement("li");
+	// 	function newDiv(brian) {
+	// 		var newDiv = document.createElement("div");
+	// 		$(newDiv).addClass(brian);
+	// 	};
+	// 	// $(newLi).text(newItemName);
+	// 	newDiv("remove");
+
+	// 	$(newLi).append($(newDiv));
+
+	// 	$("#uncategorized-ul").append($(newLi));
+
+	// 	var lastItem = $("#uncategorized-ul").last();
+
+	// 	$(lastItem).find(".text").text(newItemName);
+	// 	console.log( lastItem );
+
+	// 	$("#input").val("");
+	// });
 
 	$(".remove").on("click", 
 		function() {
