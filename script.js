9717e524194ff100f245000873afb5bfffefead0
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-04 13:57:26 -0400

    Cat Save III
    Categories sort properly, BUT ONLY on the first click.  After that,
    nothing happens.  Resolved issue of all children values changing

--- script.js
+++ script.js
@@ -3,18 +3,13 @@ $(document).ready(function() {
 	alert("hello world");
 
 	function addNewItem(catId) {
-		var newItemName = $("#input").val();
 		var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div><div class="text">newItemName goes here</div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div>';
 		$(catId).append(newItem); 
-		var lastItem = $(catId).last();
+
+		var newItemName = $("#input").val();
+		var lastItem = $(catId).children().last();
 		lastItem.find(".text").text(newItemName);
 	}
-
-	// function changeCat(categoryId) {
-	// 	var lineItem = $(this).closest("li");
-	// 	lineItem.remove();
-	// 	$(categoryId).append(lineItem);	
-	// }
 
 	$("header .cat-icons").on("click", "i", function(){
 		$(".cat-icons i").removeClass("cat-select");
@@ -29,61 +24,39 @@ $(document).ready(function() {
 		// if this has class of i-fnv, remove the line and 
 		// append it to the fnv ul
 
-		// var lineItem = $(this).closest("li");
 		var lineItem = $(this).closest("li");
 
-		function changeCat(cattyId) {
+		function changeCat(catId) {
 			lineItem.remove();
-			$(cattyId).append(lineItem);
+			$(catId).append(lineItem);
 		}
 
 		if($(this).hasClass("i-fnv")) {
 			changeCat("#fnv-ul");
+		} else if($(this).hasClass("i-dairy")) {
+			changeCat("#dairy-ul");
+		} else if($(this).hasClass("i-meat")) {
+			changeCat("#meat-ul");
+		} else if($(this).hasClass("i-bng")) {
+			changeCat("#bng-ul");
+		} else if($(this).hasClass("i-snacks")) {
+			changeCat("#snacks-ul");
+		} else if($(this).hasClass("i-canned")) {
+			changeCat("#canned-ul");
+		} else if($(this).hasClass("i-spices")) {
+			changeCat("#spices-ul");
+		} else if($(this).hasClass("i-hhg")) {
+			changeCat("#hhg-ul");
+		} else if($(this).hasClass("i-other")) {
+			changeCat("#other-ul");
 		};
-
-		// if($(this).hasClass("i-fnv")) {
-		// 	lineItem.remove();
-		// 	$("#fnv-ul").append(lineItem);
-		// };
-
-		// var lineItem = $(this).closest("li");
-		// lineItem.remove();
-		// $("#fnv-ul").append(lineItem);	
-
-		// if($(this).closest("li").find("i").first().hasClass("cat-select")) {
-		// 	$("#fnv-ul").append(lineItem);
-
-
-		// };
-
-
-		// 	("li .cat-icons i:first-child").hasClass("cat-select")) {
-		// 	addNewItem("#fnv-ul");
-		// } else if($("header .cat-icons i:nth-child(2)").hasClass("cat-select")) {
-		// 	addNewItem("#dairy-ul");
-		// } else if($("header .cat-icons i:nth-child(3)").hasClass("cat-select")) {
-		// 	addNewItem("#meat-ul");
-		// } else if($("header .cat-icons i:nth-child(4)").hasClass("cat-select")) {
-		// 	addNewItem("#bng-ul");
-		// } else if($("header .cat-icons i:nth-child(5)").hasClass("cat-select")) {
-		// 	addNewItem("#snacks-ul");
-		// } else if($("header .cat-icons i:nth-child(6)").hasClass("cat-select")) {
-		// 	addNewItem("#canned-ul");
-		// } else if($("header .cat-icons i:nth-child(7)").hasClass("cat-select")) {
-		// 	addNewItem("#spices-ul");
-		// } else if($("header .cat-icons i:nth-child(8)").hasClass("cat-select")) {
-		// 	addNewItem("#hhg-ul");
-		// } else if($("header .cat-icons i:nth-child(9)").hasClass("cat-select")) {
-		// 	addNewItem("#other-ul");
-		// } 
-
-		// $("li .cat-icons i").removeClass("cat-select");
 
 	});
 
 	$("#add").on("click", function() {
 		if($("header i").hasClass("cat-select") === false) {
 			addNewItem("#uncategorized-ul");
+			addText("#uncategorized-ul");
 		} else if($("header .cat-icons i:first-child").hasClass("cat-select")) {
 			addNewItem("#fnv-ul");
 		} else if($("header .cat-icons i:nth-child(2)").hasClass("cat-select")) {
@@ -105,8 +78,9 @@ $(document).ready(function() {
 		} 
 
 
-		$("#input").val("");
+	
 		$("header .cat-icons i").removeClass("cat-select");
+		$("#input").val("");
 	});
 
 	// $("#input").on("keyup", function(event) {
@@ -121,32 +95,6 @@ $(document).ready(function() {
 		};
 	});
 
-
-
-
-
-
-	// $("#add").on("click", function() {
-	// 	var newItemName = $("#input").val();
-	// 	var newLi = document.createElement("li");
-	// 	function newDiv(brian) {
-	// 		var newDiv = document.createElement("div");
-	// 		$(newDiv).addClass(brian);
-	// 	};
-	// 	// $(newLi).text(newItemName);
-	// 	newDiv("remove");
-
-	// 	$(newLi).append($(newDiv));
-
-	// 	$("#uncategorized-ul").append($(newLi));
-
-	// 	var lastItem = $("#uncategorized-ul").last();
-
-	// 	$(lastItem).find(".text").text(newItemName);
-	// 	console.log( lastItem );
-
-	// 	$("#input").val("");
-	// });
 
 	$(".remove").on("click", 
 		function() {
