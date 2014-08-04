e73db468aa876b39c9c4c93142b60efdafa6f2b8
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-04 16:06:01 -0400

    transform h1
    Rotate h1 360deg

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 
-	alert("hello world");
+	// alert("hello world");
+
+	$("h1").css({"transform": "rotate(360deg)"});
 
 	function addNewItem(catId) {
 		var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div><div class="text">newItemName goes here</div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div>';
