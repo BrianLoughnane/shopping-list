acf29be0c299077669c746ab258f07966fe9db2e
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-05 12:08:08 -0400

    Good lead on bug fix
    nth-child issues

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ $(document).ready(function() {
 	// 	};
 	// });	
 
+
 	// Header Effect
 
 	$("h1").css({"transform": "rotateY(360deg)"});
 
 	$("body").animate({"opacity": "1"});
+
+
 
 
 	// Use the enter key to add items
@@ -21,64 +24,14 @@ $(document).ready(function() {
 		};
 	});
 
-	// Create a new Item and add it to specified category
-
-
-
-	// Add item to a list based on if the header icon is selected
-
-	$("#add").on("click", function() {
-
-		function addNewItem(catId) {
-
-			var newItemName = $.trim($("#input").val());
-			var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div><div class="text">'+ newItemName +'</div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div></div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div></li>';
-
-			if(newItemName !== "") {
-			$(catId).append(newItem); 
-			$("header .cat-icons i").removeClass("cat-select");
-			$("#input").val("");
-			};
-
-		}
-
-		if($("header i").hasClass("cat-select") === false) {
-			addNewItem("#uncategorized-ul");
-		} else if($("header .cat-icons i:first-child").hasClass("cat-select")) {
-			addNewItem("#fnv-ul");
-		} else if($("header .cat-icons i:nth-child(2)").hasClass("cat-select")) {
-			addNewItem("#dairy-ul");
-		} else if($("header .cat-icons i:nth-child(3)").hasClass("cat-select")) {
-			addNewItem("#meat-ul");
-		} else if($("header .cat-icons i:nth-child(4)").hasClass("cat-select")) {
-			addNewItem("#bng-ul");
-		} else if($("header .cat-icons i:nth-child(5)").hasClass("cat-select")) {
-			addNewItem("#snacks-ul");
-		} else if($("header .cat-icons i:nth-child(6)").hasClass("cat-select")) {
-			addNewItem("#canned-ul");
-		} else if($("header .cat-icons i:nth-child(7)").hasClass("cat-select")) {
-			addNewItem("#spices-ul");
-		} else if($("header .cat-icons i:nth-child(8)").hasClass("cat-select")) {
-			addNewItem("#hhg-ul");
-		} else if($("header .cat-icons i:nth-child(9)").hasClass("cat-select")) {
-			addNewItem("#other-ul");
-		}; 
-	});
-
-
-
 
 
 	// Header Icons -- Assign a category class and add item to the list
 
-	$("header .cat-icons")
-	.on("click", "i", 
-		function() {
+	$("header .cat-icons").on("click", "i", function() {
 			$(".cat-icons i").removeClass("cat-select");
 			$(this).addClass("cat-select");
-			if($("#input").val() !== "") {
-				$("#add").click();
-			};
+			$("#add").click();
 		}
 	);
 	// .on("mouseenter", "i", 
@@ -153,6 +106,49 @@ $(document).ready(function() {
 			changeCat("#other-ul");
 		};
 
+	});
+
+
+
+	// Add item 
+
+
+	$("#add").on("click", function() {
+
+		function addNewItem(catId) {
+
+			var newItemName = $.trim($("#input").val());
+			var newItem = '<li class="item"><div class="remove"><i class="i-remove"></i></div><div><div class="text">'+ newItemName +'</div><div class="cat-icons"><i class="i-fnv"></i><i class="i-dairy"></i><i class="i-meat"></i><i class="i-bng"></i><i class="i-snacks"></i><i class="i-canned"></i><i class="i-spices"></i><i class="i-hhg"></i><i class="i-other"></i></div></div><div class="check-uncheck"><i class="i-check"></i><i class="i-uncheck hide"></i></div></li>';
+
+			if(newItemName !== "") {
+				$(catId).append(newItem); 
+				$("header .cat-icons i").removeClass("cat-select");
+				$("#input").val("");
+			};
+
+		}
+
+		if($("header i").hasClass("cat-select") === false) {
+			addNewItem("#uncategorized-ul");
+		} else if($("header .cat-icons i:first-child").hasClass("cat-select")) {
+			addNewItem("#fnv-ul");
+		} else if($("header .cat-icons i:nth-child(2)").hasClass("cat-select")) {
+			addNewItem("#dairy-ul");
+		} else if($("header .cat-icons i:nth-child(3)").hasClass("cat-select")) {
+			addNewItem("#meat-ul");
+		} else if($("header .cat-icons i:nth-child(4)").hasClass("cat-select")) {
+			addNewItem("#bng-ul");
+		} else if($("header .cat-icons i:nth-child(5)").hasClass("cat-select")) {
+			addNewItem("#snacks-ul");
+		} else if($("header .cat-icons i:nth-child(6)").hasClass("cat-select")) {
+			addNewItem("#canned-ul");
+		} else if($("header .cat-icons i:nth-child(7)").hasClass("cat-select")) {
+			addNewItem("#spices-ul");
+		} else if($("header .cat-icons i:nth-child(8)").hasClass("cat-select")) {
+			addNewItem("#hhg-ul");
+		} else if($("header .cat-icons i:nth-child(9)").hasClass("cat-select")) {
+			addNewItem("#other-ul");
+		}; 
 	});
 
 
